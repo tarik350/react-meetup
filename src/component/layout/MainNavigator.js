f1367a5195a1395d8a470202705fda2bd27aaffa
tarik350 <80385670+tarik350@users.noreply.github.com>
2023-03-25 00:09:12 +0300

    done

--- src/component/layout/MainNavigator.js
+++ src/component/layout/MainNavigator.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import classes from "./MainNavigator.module.css";
+import { useContext } from "react";
+import FavoriteContext from "../../store/favorite-context";
 
 const MainNavigator = () => {
+  const favortieContext = useContext(FavoriteContext);
+  const favCount = favortieContext.totalFavorites;
+
   return (
     <header className={classes.header}>
       <div className={classes.logo}>React meetups</div>
@@ -16,6 +21,7 @@ const MainNavigator = () => {
           </li>
           <li>
             <Link to="/favorites">My favorites</Link>
+            <span className={classes.badge}>{favCount}</span>
           </li>
         </ul>
       </nav>
--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,5 +1,6 @@
 import Card from "../component/ui/Card";
 import { useContext } from "react";
+import classes from "./FavoritePage.module.css";
 import FavoriteContext from "../store/favorite-context";
 import MeetUpItems from "../component/meetups/MeetUpItems";
 import MeetupList from "../component/meetups/MeetupList";
@@ -7,9 +8,22 @@ import MeetupList from "../component/meetups/MeetupList";
 const FavoritesPage = () => {
   const favoriteContext = useContext(FavoriteContext);
   const favorites = favoriteContext.favorites;
+
+  let content;
+
+  if (favoriteContext.totalFavorites === 0) {
+    content = (
+      <p className={classes.verticalCenter}>
+        your fav list is empty try adding some
+      </p>
+    );
+  } else {
+    content = <MeetupList meetups={favorites} />;
+  }
   return (
     <div>
-      <MeetupList meetups={favorites} />
+      <h1>My Favorites</h1>
+      {content}
     </div>
   );
 };
